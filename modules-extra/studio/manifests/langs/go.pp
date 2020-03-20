9f34686dd1c0f1893e1acea49f5e197861b543a8
Cliffano Subagio <cliffano@gmail.com>
2020-03-20 11:00:20 +1100

    Add Packer 1.5.4 .

--- modules-extra/studio/manifests/langs/go.pp
+++ modules-extra/studio/manifests/langs/go.pp
@@ -10,6 +10,10 @@ class studio::langs::go (
     download_cache_dir => '/tmp/hashicorp-cache/',
   }
 
+  class { 'hashicorp::packer':
+    version => '1.5.4',
+  }
+
   class { 'hashicorp::terraform':
     version => '0.12.21',
   }

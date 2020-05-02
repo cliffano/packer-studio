d3c579a57acde5a35458336e21a247650ce8cbd4
Cliffano Subagio <cliffano@gmail.com>
2020-05-02 14:37:50 +1000

    Upgrade node.js to version 13.x .

--- modules-extra/studio/manifests/langs/javascript.pp
+++ modules-extra/studio/manifests/langs/javascript.pp
@@ -1,11 +1,8 @@
 class studio::langs::javascript (
 ) {
 
-  class { '::nodejs':
-    manage_package_repo       => false,
-    nodejs_dev_package_ensure => 'present',
-    npm_package_ensure        => 'present',
-    repo_url_suffix           => '10.x',
+  class { 'nodejs':
+    repo_url_suffix => '13.x',
   }
 
 }

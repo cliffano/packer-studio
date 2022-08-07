7c21f9b11082dd93e3bbe57edaa57746cf6d2729
Cliffano Subagio <cliffano@gmail.com>
2022-08-07 18:08:40 +1000

    Add paths for listing post provision software info. Add versions of top level langs.

--- provisioners/info.sh
+++ provisioners/info.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -o errexit
 
+export PATH=/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:$PATH
+
 echo "****************************************"
 echo "* Provisioned software info"
 echo "****************************************"
@@ -8,5 +10,9 @@ echo "PATH: $PATH"
 echo "puppet version: $(puppet --version)"
 echo "facter version: $(facter --version)"
 echo "hiera version: $(hiera --version)"
+echo "go version: $(go --version)"
+echo "java version: $(java --version)"
 echo "node.js version: $(node --version)"
+echo "python version: $(python --version)"
 echo "ruby version: $(ruby --version)"
+echo "rust version: $(rust --version)"

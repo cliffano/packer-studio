8c6c3284844cf6ef84a8df9671ca6e8ac3b48141
Cliffano Subagio <cliffano@gmail.com>
2022-08-07 20:09:39 +1000

    Update info scrip with more libraries versions.

--- provisioners/info.sh
+++ provisioners/info.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -o errexit
 
-export PATH=/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin:/usr/local/go/bin:/root/.cargo/bin/:/usr/local/sbin:/usr/local/bin:$PATH
 
 echo "****************************************"
 echo "* Provisioned software info"
@@ -10,9 +10,15 @@ echo "PATH: $PATH"
 echo "puppet version: $(puppet --version)"
 echo "facter version: $(facter --version)"
 echo "hiera version: $(hiera --version)"
-echo "go version: $(go --version)"
-echo "java version: $(java --version)"
+echo "go version: $(go version)"
+echo "SDKMAN version: $(sdk version)"
 echo "node.js version: $(node --version)"
 echo "python version: $(python --version)"
+echo "ansible version: $(ansible --version)"
+echo "awscli version: $(aws --version)"
+echo "checkov version: $(checkov --version)"
 echo "ruby version: $(ruby --version)"
-echo "rust version: $(rust --version)"
+echo "gem version: $(gem --version)"
+echo "bundler version: $(bundler --version)"
+echo "rspec version: $(rspec --version)"
+echo "rustup version: $(rustup --version)"

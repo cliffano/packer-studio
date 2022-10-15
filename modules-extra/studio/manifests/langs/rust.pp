79838735212d3d4b8d319913f87aa6b1ddb7e036
Cliffano Subagio <cliffano@gmail.com>
2022-10-15 19:02:13 +1100

    Split cargo installs for better log visibility.

--- modules-extra/studio/manifests/langs/rust.pp
+++ modules-extra/studio/manifests/langs/rust.pp
@@ -6,7 +6,19 @@ class studio::langs::rust (
   }
 
   # https://betterprogramming.pub/5-cli-tools-made-with-rust-to-improve-already-popular-tools-506af07b6d54
-  exec { 'cargo install bat ripgrep du-dust bottom exa':
+  exec { 'cargo install bat':
+    path => ['/root/.cargo/bin/', '/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+  exec { 'cargo install ripgrep':
+    path => ['/root/.cargo/bin/', '/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+  exec { 'cargo install du-dust':
+    path => ['/root/.cargo/bin/', '/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+  exec { 'cargo install bottom':
+    path => ['/root/.cargo/bin/', '/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+  exec { 'cargo install exa':
     path => ['/root/.cargo/bin/', '/bin', '/usr/bin', '/sbin', '/usr/sbin'],
   }
 

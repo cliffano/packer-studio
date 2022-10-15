8b765c2f55856a8d06babf66d7a75adab917b485
Cliffano Subagio <cliffano@gmail.com>
2022-10-15 16:15:44 +1100

    Install gcc due to linker cc on rustup.

--- provisioners/init.sh
+++ provisioners/init.sh
@@ -8,7 +8,7 @@ apt-get autoremove
 apt-get autoclean
 
 # Install common utilities
-apt-get install -y apt-utils build-essential
+apt-get install -y apt-utils build-essential gcc
 
 # Install Puppet, adapted from
 # https://github.com/puppetlabs/puppet-in-docker/blob/master/puppet-agent-ubuntu/Dockerfile

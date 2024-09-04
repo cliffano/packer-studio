2386e860af9ea41eec87ede26c299bfa3d8020fe
Cliffano Subagio <cliffano@gmail.com>
2024-09-04 20:58:24 +1000

    Fix publish GH Action workflow

--- .github/actions/publish-docker-studio-action/entrypoint.sh
+++ .github/actions/publish-docker-studio-action/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker --version
 source /home/.virtualenvs/py36/bin/activate
-make clean deps lint build-docker-studio
+make clean stage deps lint build-docker-studio
 cat logs/packer-docker-studio.log
 echo "${DOCKERHUB_TOKEN}" | docker login --username cliffano --password-stdin
 docker inspect cliffano/studio
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Fixed
+- Fix publish GH Action workflow
+
 ## 2.0.0 - 2024-09-04
 ### Added
 - Add Packer plugins for Docker and Ansible
